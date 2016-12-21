8181515fe7eb73241028640f85b033ba85a935cb
Sergey Ponomarenko <xto3na@gmail.com>
2016-12-21 23:22:50 +0200

    Structure of site is ready.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,21 @@ import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
+import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 const appRoutes: Routes = [
-    {
-        path: '',
-        component: GameComponent
-    }
+  {
+    path: '',
+    component: GameComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
@@ -23,7 +31,10 @@ const appRoutes: Routes = [
     AppComponent,
     GameComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MenuComponent,
+    AboutComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
