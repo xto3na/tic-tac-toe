91217e49ae326b72ab03edeb3af5863a2afbf68c
Sergey Ponomarenko <xto3na@gmail.com>
2016-12-24 04:04:13 +0200

    After some refactoring.

--- src/app/line.ts
+++ src/app/line.ts
@@ -2,10 +2,7 @@ import { Cell } from "./cell";
 
 export class Line {
   id:number;
-  isEnable:boolean = true;
-  priority:number = 0;
-  countCompSymbol:number = 0;
-  lineCells:Cell[] = new Array<Cell>();
+  lineCells:Cell[] = [];
   constructor (values: Object = {}) {
     Object.assign( this, values );
   }
